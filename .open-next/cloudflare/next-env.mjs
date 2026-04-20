b9aebac0b8c341cf7da4d6b8a524a48837664082
Nguyen Manh Cuong <nmcuong.degoholding@gmail.com>
2026-04-20 17:50:00 +0700

    Cấu hình chuẩn OpenNext và Wrangler JSONC để chèn biến môi trường Supabase thẳng vào Cloudflare Worker nhằm sửa lỗi Internal Server Error

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"SITE_NAME":"Gia Phả OS","NEXT_PUBLIC_SUPABASE_URL":"https://njxsnnwnklmfzzawovou.supabase.co","NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY":"sb_publishable_0_gvfHmIVjDoQf3KGQGl_w_p3OCDGbP"};
+export const development = {"SITE_NAME":"Gia Phả OS","NEXT_PUBLIC_SUPABASE_URL":"https://njxsnnwnklmfzzawovou.supabase.co","NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY":"sb_publishable_0_gvfHmIVjDoQf3KGQGl_w_p3OCDGbP"};
+export const test = {};
---
+++ .open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_missing-db-config_page_actions_0-.n3za.js
@@ -0,0 +1,3 @@
+module.exports=[59489,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_missing-db-config_page_actions_0-.n3za.js.map
